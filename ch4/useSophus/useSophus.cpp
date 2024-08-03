0da5ad0d33796ec145a70e643d69cf858d3fbb84
Sim <sim30217@gmail.com>
2024-08-03 17:04:59 +0900

    update : modern cpp

--- ch4/useSophus/useSophus.cpp
+++ ch4/useSophus/useSophus.cpp
@@ -1,64 +1,74 @@
 #include <iostream>
 #include <cmath>
-using namespace std; 
-
 #include <Eigen/Core>
 #include <Eigen/Geometry>
+#include <sophus/so3.hpp>
+#include <sophus/se3.hpp>
 
-#include "sophus/so3.h"
-#include "sophus/se3.h"
+// Overload the << operator for Sophus::SO3d
+std::ostream& operator<<(std::ostream& out, const Sophus::SO3d& so3) {
+    Eigen::Quaterniond q = so3.unit_quaternion();
+    out << "SO3 [w, x, y, z]: [" << q.w() << ", " << q.x() << ", " << q.y() << ", " << q.z() << "]";
+    return out;
+}
 
-int main( int argc, char** argv )
-{
-    // 沿Z轴转90度的旋转矩阵
-    Eigen::Matrix3d R = Eigen::AngleAxisd(M_PI/2, Eigen::Vector3d(0,0,1)).toRotationMatrix();
+// Overload the << operator for Sophus::SE3d
+std::ostream& operator<<(std::ostream& out, const Sophus::SE3d& se3) {
+    Eigen::Quaterniond q = se3.unit_quaternion();
+    Eigen::Vector3d t = se3.translation();
+    out << "SE3 [translation: " << t.transpose() << ", quaternion: " << q.w() << ", " << q.x() << ", " << q.y() << ", " << q.z() << "]";
+    return out;
+}
+
+int main() {
+    // Rotation matrix for a 90 degree rotation around Z axis
+    Eigen::Matrix3d R = Eigen::AngleAxisd(M_PI / 2, Eigen::Vector3d(0, 0, 1)).toRotationMatrix();
     
-    Sophus::SO3 SO3_R(R);               // Sophus::SO(3)可以直接从旋转矩阵构造
-    Sophus::SO3 SO3_v( 0, 0, M_PI/2 );  // 亦可从旋转向量构造
-    Eigen::Quaterniond q(R);            // 或者四元数
-    Sophus::SO3 SO3_q( q );
-    // 上述表达方式都是等价的
-    // 输出SO(3)时，以so(3)形式输出
-    cout<<"SO(3) from matrix: "<<SO3_R<<endl;
-    cout<<"SO(3) from vector: "<<SO3_v<<endl;
-    cout<<"SO(3) from quaternion :"<<SO3_q<<endl;
+    Sophus::SO3d SO3_R(R);                // Construct from rotation matrix
+    Sophus::SO3d SO3_v = Sophus::SO3d::exp(Eigen::Vector3d(0, 0, M_PI / 2));   // Construct from rotation vector
+    Eigen::Quaterniond q(R);              // Construct from quaternion
+    Sophus::SO3d SO3_q(q);
     
-    // 使用对数映射获得它的李代数
+    // All representations are equivalent
+    std::cout << "SO(3) from matrix: " << SO3_R << std::endl;
+    std::cout << "SO(3) from vector: " << SO3_v << std::endl;
+    std::cout << "SO(3) from quaternion: " << SO3_q << std::endl;
+    
+    // Logarithmic map to get the Lie algebra
     Eigen::Vector3d so3 = SO3_R.log();
-    cout<<"so3 = "<<so3.transpose()<<endl;
-    // hat 为向量到反对称矩阵
-    cout<<"so3 hat=\n"<<Sophus::SO3::hat(so3)<<endl;
-    // 相对的，vee为反对称到向量
-    cout<<"so3 hat vee= "<<Sophus::SO3::vee( Sophus::SO3::hat(so3) ).transpose()<<endl; // transpose纯粹是为了输出美观一些
+    std::cout << "so3 = " << so3.transpose() << std::endl;
+    // Hat operator to convert vector to skew-symmetric matrix
+    std::cout << "so3 hat =\n" << Sophus::SO3d::hat(so3) << std::endl;
+    // Vee operator to convert skew-symmetric matrix back to vector
+    std::cout << "so3 hat vee = " << Sophus::SO3d::vee(Sophus::SO3d::hat(so3)).transpose() << std::endl;
+
+    // Update model with a small perturbation
+    Eigen::Vector3d update_so3(1e-4, 0, 0); // Small update
+    Sophus::SO3d SO3_updated = Sophus::SO3d::exp(update_so3) * SO3_R;
+    std::cout << "SO3 updated = " << SO3_updated << std::endl;
+
+    std::cout << "************ Separator *************" << std::endl;
+
+    // SE(3) operations
+    Eigen::Vector3d t(1, 0, 0);           // Translation vector along X axis
+    Sophus::SE3d SE3_Rt(R, t);            // Construct SE(3) from rotation matrix and translation
+    Sophus::SE3d SE3_qt(q, t);            // Construct SE(3) from quaternion and translation
+    std::cout << "SE3 from R,t= " << std::endl << SE3_Rt << std::endl;
+    std::cout << "SE3 from q,t= " << std::endl << SE3_qt << std::endl;
     
-    // 增量扰动模型的更新
-    Eigen::Vector3d update_so3(1e-4, 0, 0); //假设更新量为这么多
-    Sophus::SO3 SO3_updated = Sophus::SO3::exp(update_so3)*SO3_R;
-    cout<<"SO3 updated = "<<SO3_updated<<endl;
-    
-    /********************萌萌的分割线*****************************/
-    cout<<"************我是分割线*************"<<endl;
-    // 对SE(3)操作大同小异
-    Eigen::Vector3d t(1,0,0);           // 沿X轴平移1
-    Sophus::SE3 SE3_Rt(R, t);           // 从R,t构造SE(3)
-    Sophus::SE3 SE3_qt(q,t);            // 从q,t构造SE(3)
-    cout<<"SE3 from R,t= "<<endl<<SE3_Rt<<endl;
-    cout<<"SE3 from q,t= "<<endl<<SE3_qt<<endl;
-    // 李代数se(3) 是一个六维向量，方便起见先typedef一下
-    typedef Eigen::Matrix<double,6,1> Vector6d;
+    // Lie algebra se(3) is a six-dimensional vector
+    using Vector6d = Eigen::Matrix<double, 6, 1>;
     Vector6d se3 = SE3_Rt.log();
-    cout<<"se3 = "<<se3.transpose()<<endl;
-    // 观察输出，会发现在Sophus中，se(3)的平移在前，旋转在后.
-    // 同样的，有hat和vee两个算符
-    cout<<"se3 hat = "<<endl<<Sophus::SE3::hat(se3)<<endl;
-    cout<<"se3 hat vee = "<<Sophus::SE3::vee( Sophus::SE3::hat(se3) ).transpose()<<endl;
-    
-    // 最后，演示一下更新
-    Vector6d update_se3; //更新量
-    update_se3.setZero();
-    update_se3(0,0) = 1e-4d;
-    Sophus::SE3 SE3_updated = Sophus::SE3::exp(update_se3)*SE3_Rt;
-    cout<<"SE3 updated = "<<endl<<SE3_updated.matrix()<<endl;
-    
+    std::cout << "se3 = " << se3.transpose() << std::endl;
+    // Hat and vee operators
+    std::cout << "se3 hat = " << std::endl << Sophus::SE3d::hat(se3) << std::endl;
+    std::cout << "se3 hat vee = " << Sophus::SE3d::vee(Sophus::SE3d::hat(se3)).transpose() << std::endl;
+
+    // Update SE(3) with a small perturbation
+    Vector6d update_se3 = Vector6d::Zero();
+    update_se3(0, 0) = 1e-4;
+    Sophus::SE3d SE3_updated = Sophus::SE3d::exp(update_se3) * SE3_Rt;
+    std::cout << "SE3 updated = " << std::endl << SE3_updated.matrix() << std::endl;
+
     return 0;
 }
